5ca9aef7312d66e1c61c20bae037402ce39beaa8
lanexl <56983001+lanexl@users.noreply.github.com>
2022-02-05 14:32:25 -0500

    Worked on playerm

--- Wacky Chess/Assets/Scripts/Player.cs
+++ Wacky Chess/Assets/Scripts/Player.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Variable to determine which player this is
+    // Player 1 = white
+    // Player 2 = black
+    public int playerID;
+    List<Piece> pieceList = new List<Piece>();
+
     // Start is called before the first frame update
     void Start()
     {
-        // Variable to determine which player this is
-        // Player 1 = white
-        // Player 2 = black
-        int playerID;
-        Piece[] pieces;
+        // Create and add all 16 pieces to the piecelist
+        pieceList.Add(new ZigZagger());
+        pieceList.Add(new ZigZagger());
+        pieceList.Add(new ChainKiller());
+        pieceList.Add(new ChainKiller());
+        pieceList.Add(new Cannon());
+        pieceList.Add(new Cannon());
+        pieceList.Add(new SuicideBomber());
+        pieceList.Add(new Vip());
+        for (int i = 0; i < 8; i++)
+        {
+            pieceList.Add(new CannonFodder());
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Method to remove a piece that has been captured from this players piece list. Piece will be destroyed and then removed from the list
+    /// </summary>
+    public void removePiece(Piece pieceToRemove)
+    {
+        pieceList.Remove(pieceToRemove);
     }
 }
